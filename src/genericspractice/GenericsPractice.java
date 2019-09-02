204184753284b8c08fb1de50d4182fcdd33dd1d4
James Mitchell <jamesmitchelldesign1@gmail.com>
2019-09-02 15:19:55 +1200

    Completed Challenge

--- src/genericspractice/GenericsPractice.java
+++ src/genericspractice/GenericsPractice.java
@@ -5,9 +5,12 @@
  */
 package genericspractice;
 
+import java.util.ArrayList;
+
 /**
- *
- * @author 92020474
+ * GenericsPractice.java
+ * @author James Mitchell 92020474
+ * Date: 2/09/19
  */
 public class GenericsPractice {
 
@@ -15,7 +18,55 @@ public class GenericsPractice {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ArrayList<String> list = new ArrayList<>();
+        list.add("One");
+        list.add("Two");
+        list.add("Three");
+        list.add("Four");
+        list.add("Three");
+        list.add("Two");
+        list.add("Five");
+        
+        ArrayList<String> noDups = removeDuplicates(list);
+        
+        System.out.println(noDups);
+    }
+    
+    /**
+     *
+     * @param <E>
+     * @param list
+     * @return
+     */
+    public static <E extends Comparable<E>> ArrayList<E> removeDuplicates(ArrayList<E> list)
+    {
+        boolean foundDuplicate = false;
+        if(list.isEmpty())
+            return list;
+
+        ArrayList<E> newList = new ArrayList<>();
+        newList.add(list.get(0));
+
+        for(int i = 1; i < list.size(); i++)
+        {
+            for(int j = 0; j < newList.size(); j++)
+            {
+                if(list.get(i).compareTo(newList.get(j))==0)
+                {
+                    foundDuplicate = true;
+                    break;
+                }
+            }
+            
+            if(foundDuplicate != true)
+            {
+                newList.add(list.get(i));
+            }
+            
+            foundDuplicate = false;   
+        }
+        
+        return newList;
     }
     
 }
